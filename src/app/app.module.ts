d52fb71e6dfdd4b43ddb42b800faa9c1b9443cd7
ofirf <ofirf@sentinel.com>
2018-07-14 23:00:34 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CheckboxModule } from '../../projects/ui-lib/src/lib/checkbox-module/checkbox.module';
+import { ButtonModule } from '../../projects/ui-lib/src/lib/button-module/button-module.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    CheckboxModule
+    CheckboxModule,
+    ButtonModule
   ],
   declarations: [
     AppComponent,
