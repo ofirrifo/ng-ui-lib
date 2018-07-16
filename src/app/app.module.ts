ff8fd68a4340f10ef3ba0be46306c5676c25119e
ofirf <ofirf@sentinel.com>
2018-07-16 19:01:09 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CheckboxModule } from '../../projects/ui-lib/src/lib/checkbox-module/checkbox.module';
 import { ButtonModule } from '../../projects/ui-lib/src/lib/button-module/button-module.module';
-import { LeftPanelModule } from './fetures/leftn-pannel/left-panel.module';
+import { LeftPanelModule } from './fetures/left-panel/left-panel.module';
 import { AccordionModule } from '../../projects/ui-lib/src/lib/accordion-module/accordion.module';
 
 
