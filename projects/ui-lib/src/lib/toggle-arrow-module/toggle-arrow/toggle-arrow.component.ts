fa2482a389c5fd22d8031372c998ffa6bc998c2f
ofirf <ofirf@sentinel.com>
2018-07-17 20:30:15 +0300

    first commit

--- projects/ui-lib/src/lib/toggle-arrow-module/toggle-arrow/toggle-arrow.component.ts
+++ projects/ui-lib/src/lib/toggle-arrow-module/toggle-arrow/toggle-arrow.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ui-toggle-arrow',
   templateUrl: './toggle-arrow.component.html',
-  styleUrls: ['./toggle-arrow.component.scss']
+  styleUrls: ['./toggle-arrow.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ToggleArrowComponent implements OnInit {
+
+  _open = false;
+  @Input()
+  set open(open: boolean) {
+    this._open = open;
+  }
 
   constructor() { }
 
